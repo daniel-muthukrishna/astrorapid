64fb1dfc17b8cd8dc79af472ba5ca139ba68ddbd
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2019-01-23 12:04:29 +0000

    Moved saved training set files to training_set_dir

--- astrorapid/prepare_arrays.py
+++ astrorapid/prepare_arrays.py
@@ -185,7 +185,8 @@ class PrepareTrainingSetArrays(PrepareArrays):
         if not os.path.exists(self.training_set_dir):
             os.makedirs(self.training_set_dir)
 
-    def get_saved_light_curves_from_database(self, fpath_saved_lc):
+    @staticmethod
+    def get_saved_light_curves_from_database(fpath_saved_lc):
         """ Get objids from hdf5 file
 
         Parameters
@@ -209,9 +210,10 @@ class PrepareTrainingSetArrays(PrepareArrays):
         return objids, fpath_saved_lc
 
     def prepare_training_set_arrays(self, fpath_saved_lc, otherchange=''):
-        savepath = "X_{}ag{}_ci{}_fp{}_z{}_b{}_var{}.npy".format(otherchange, self.aggregate_classes,
+        savepath = os.path.join(self.training_set_dir, "X_{}ag{}_ci{}_fp{}_z{}_b{}_var{}.npy".format(otherchange, self.aggregate_classes,
                                                                  self.contextual_info, os.path.basename(fpath_saved_lc),
-                                                                 self.zcut, self.bcut, self.variablescut)
+                                                                 self.zcut, self.bcut, self.variablescut))
+
         if self.reread is True or not os.path.isfile(os.path.join(self.training_set_dir, savepath)):
             objids, self.fpath = self.get_saved_light_curves_from_database(fpath_saved_lc)
             nobjects = len(objids)
