9b4a9ca2b2135b3c3cecedfa132fbe6cc510656f
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2019-07-22 20:52:13 -0700

    API documentation only showing locally not showing on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, 'astrorapid')
 sys.path.insert(0, 'astrorapid/read_from_database')
 sys.path.insert(0, '../astrorapid')
