bd528d608cbc7fd17ffdf29eda127dc372303b20
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2020-04-01 19:09:05 +0100

    Fixed incorrectly printing "Invalid keras model. Using default model..."

--- astrorapid/classify.py
+++ astrorapid/classify.py
@@ -100,7 +100,7 @@ class Classify(object):
         if model_filepath != '' and os.path.exists(model_filepath):
             self.model_filepath = model_filepath
         else:
-            if not os.path.exists(model_filepath):
+            if not (model_filepath == '' and os.path.exists(model_filepath)):
                 print("Invalid keras model. Using default model...")
             self.model_filepath = os.path.join(SCRIPT_DIR, filename)
             if not os.path.exists(self.model_filepath):
