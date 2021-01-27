72fefc652293de7909349413b81aa622db59db5b
Deep Chatterjee <deep.chatterjee@ligo.org>
2021-01-27 00:24:10 -0600

    fix failing build, switch CI to github actions, merge after #10

--- setup.py
+++ setup.py
@@ -49,7 +49,15 @@ setup(name='astrorapid',
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'tensorflow', 'keras', 'astropy', 'pandas', 'extinction==0.4'],
+    install_requires=[
+        'tensorflow',
+        'keras',
+        'keras-tcn',
+        'scikit-learn',
+        'astropy',
+        'pandas',
+        'extinction==0.4'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
